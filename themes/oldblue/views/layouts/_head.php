f2acffe2fb681f711c4db523e2b11a218e717bfc
rizkyramadhan <kamil.insan@outlook.com>
2018-01-23 15:39:07 +0700

    fixing dropdown

--- themes/oldblue/views/layouts/_head.php
+++ themes/oldblue/views/layouts/_head.php
@@ -4,6 +4,8 @@
     <link rel="stylesheet" href="<?= Yii::app()->controller->staticUrl('/css/non-responsive.css'); ?>" type="text/css" />
     <link rel="stylesheet" href="<?= Yii::app()->controller->staticUrl('/css/font-awesome.min.css'); ?>" type="text/css" />
     <link rel="stylesheet" href="<?= Yii::app()->controller->staticUrl('/css/main.css'); ?>" type="text/css" />
+    <script type="text/javascript" src="<?= Yii::app()->controller->staticUrl('/js/HotKeys.js'); ?>"></script>
     <title><?php echo CHtml::encode(Yii::app()->controller->pageTitle); ?></title>
+   
     <?php ThemeManager::registerCoreScript(); ?> 
 </head>
